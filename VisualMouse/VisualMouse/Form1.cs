929a5f756f40caaee18432894ad4ba339353e626
JetRanger <2310422@qq.com>
2019-02-16 11:15:31 +0800

    Delete the DLL.

--- VisualMouse/VisualMouse/Form1.cs
+++ VisualMouse/VisualMouse/Form1.cs
@@ -5,9 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
-using Mouse;
+using System.Runtime.InteropServices;
 
 namespace VisualMouse
 {
@@ -19,12 +19,33 @@ namespace VisualMouse
             InitializeComponent();
         }
         
-        private void button1_Click(object sender, EventArgs e)
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        //移动鼠标 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        const int MOUSEEVENTF_WHEEL = 0x0800;
+
+        private void Button1_Click(object sender, EventArgs e)
         {
-            const int MOUSEEVENTF_WHEEL = 0x0800;
+            
             for (int i = 0; i < 5; i++)
             {
-                Class1.Mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -100, 1);
+                mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -100, 1);
+                Thread.Sleep(1000);
             }
         }
     }
