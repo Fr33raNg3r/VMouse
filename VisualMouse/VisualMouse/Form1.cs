9ada23e018fca04345a31ec21c7730d0070a0723
JetRanger <2310422@qq.com>
2019-02-16 10:24:48 +0800

    begin

---
+++ VisualMouse/VisualMouse/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mouse;
+
+namespace VisualMouse
+{
+    //[DllImport("user32", CharSet = CharSet.Unicode)]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int MOUSEEVENTF_WHEEL = 0x0800;
+            for (int i = 0; i < 5; i++)
+            {
+                Class1.Mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -100, 1);
+            }
+        }
+    }
+}
